8fa84ecbdded28619a25c9b1e854864c7e64411d
Basile EL AZHARI <basile.elazhari@gmail.com>
2022-10-30 20:31:00 +0100

    catalog.yml and data_science/nodes.py: corrected for X_train, X_test, y_train and y_test to save locally

--- src/kedro_tutorial/pipelines/data_science/nodes.py
+++ src/kedro_tutorial/pipelines/data_science/nodes.py
@@ -26,10 +26,10 @@ def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=parameters["test_size"], random_state=parameters["random_state"]
     )
-    return X_train, X_test, y_train, y_test
+    return pd.DataFrame(X_train), pd.DataFrame(X_test), pd.DataFrame(y_train), pd.DataFrame(y_test)
 
 
-def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> [LinearRegression, LinearRegression]:
+def train_model(X_train: pd.DataFrame, y_train: pd.DataFrame) -> [LinearRegression, LinearRegression]:
     """Trains the linear regression model.
 
     Args:
@@ -59,8 +59,7 @@ def test_predict(X_test: pd.DataFrame, model: LinearRegression) -> pd.DataFrame:
 
 
 def evaluate_model(
-    regressor: LinearRegression, X_test: pd.DataFrame, y_test: pd.Series
-) -> [Dict[str, float], Dict[str, float]]:
+    regressor: LinearRegression, X_test: pd.DataFrame, y_test: pd.DataFrame) -> [Dict[str, float], Dict[str, float]]:
     """Calculates and logs the coefficient of determination.
 
     Args:
