a1b70cdaf4ae92a0b08d816ece24a017cdea5028
Basile EL AZHARI <basile.elazhari@gmail.com>
2022-11-01 15:40:46 +0100

    user_app : inference from staging version of spaceflight_gbr model in mlflow model registry

--- src/kedro_tutorial/pipelines/user_app/nodes.py
+++ src/kedro_tutorial/pipelines/user_app/nodes.py
@@ -3,13 +3,26 @@ This is a boilerplate pipeline 'user_app'
 generated using Kedro 0.18.3
 """
 import mlflow
+import mlflow.pyfunc
 import pandas as pd
 
 
+# def predict_with_mlflow(parameters, dataset_predition: pd.DataFrame) -> pd.DataFrame:
 def predict_with_mlflow(parameters, dataset_predition: pd.DataFrame) -> pd.DataFrame:
 
-    logged_model = f'runs:/{parameters["run_id"]}/spaceflight'
-    # Load model as a PyFuncModel.
-    loaded_model = mlflow.pyfunc.load_model(logged_model)
-    # Predict on a Pandas DataFrame.
-    return loaded_model.predict(dataset_predition)
+    # logged_model = f'runs:/{parameters["run_id"]}/spaceflight'
+    # # Load model as a PyFuncModel.
+    # loaded_model = mlflow.pyfunc.load_model(logged_model)
+    # # Predict on a Pandas DataFrame.
+    # prediction = loaded_model.predict(dataset_predition)
+    # return prediction
+
+    model_name = parameters["model_name"]
+    stage = parameters["stage"]
+
+    model = mlflow.pyfunc.load_model(
+        model_uri=f"models:/{model_name}/{stage}"
+    )
+
+    return model.predict(dataset_predition)
+
